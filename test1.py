de129b420328e552677454e6990c81b8bd127ed7
William Bauer <wbauer@ibm.com@Williams-MacBook-Pro.local>
2020-02-24 17:00:53 -0600

    a

--- test1.py
+++ test1.py
@@ -1,25 +1,29 @@
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
-from IPython.display import display, clear_output
 import scipy.io.wavfile as wav
 
 
+size = 10
+num_spheres = 4
+num_steps = 1000000
+num_skip = 10000
+default_g = 1e-2
+default_k = 1e-3
+sample_rate = 44100
+
 display_graph = False
-gravity = -0.01
-default_k = 0.001
-height = 10
-width = 10
 
 
 class Sphere:
 
-    def __init__(self, pos, r=1, m=1, k=0.001):
+    def __init__(self, pos, r=1, m=1, g=default_g, k=default_k):
         self.pos = np.array(pos, dtype=np.float32)
         self.vel = np.zeros((3,), dtype=np.float32)
         self.acc = np.zeros((3,), dtype=np.float32)
         self.r = r
         self.m = m
+        self.g = g
         self.k = k
 
     def collide(self, other):
@@ -32,17 +36,17 @@ class Sphere:
         self.vel += self.acc
         self.pos += self.vel
         for i in range(3):
-            if self.pos[i] + self.r > 10:
+            if self.pos[i] + self.r > size:
                 self.vel[i] *= -1
-                self.pos[i] = 2*(10-self.r) - self.pos[i]
-            elif self.pos[i] - self.r < -10:
+                self.pos[i] = 2 * (size - self.r) - self.pos[i]
+            elif self.pos[i] - self.r < -size:
                 self.vel[i] *= -1
-                self.pos[i] = -2*(10-self.r) - self.pos[i]
+                self.pos[i] = -2 * (size - self.r) - self.pos[i]
 
 
 def do_step(spheres):
     for s in spheres:
-        s.acc = [0,-0.001,0]
+        s.acc = [0, -s.g, 0]
     for s in spheres:
         for s2 in spheres:
             if s != s2:
@@ -52,35 +56,32 @@ def do_step(spheres):
 
 def make_plot(spheres):
     fig, ax = plt.subplots()
-    ax.set_xlim((-10, 10))
-    ax.set_ylim((-10, 10))
+    ax.set_xlim((-size, size))
+    ax.set_ylim((-size, size))
     for s in spheres:
         circle = plt.Circle((s.pos[0], s.pos[1]), s.r, fill=False)
         ax.add_artist(circle)
-    clear_output(wait=True)
-    display(fig)
+    plt.show()
     plt.close()
 
-def run_steps(spheres, num_steps, skip=1):
+def run_steps(spheres, num_steps, skip=num_skip):
     audio = np.zeros((num_steps+1, 2), dtype=np.float32)
     for step in range(0, num_steps+1):
         for s in spheres:
-            #audio[step] += 0.01*s.pos[1]
-            audio[step,0] += 0.01*s.pos[1]*((10+s.pos[0])/20)
-            audio[step,1] += 0.01*s.pos[1]*((10-s.pos[0])/20)
+            dists = np.array([size + s.pos[0], size - s.pos[0]])
+            audio[step,:] += (0.01 * s.pos[1] * dists) / (2*size)
         if(step % skip == 0):
             if display_graph:
                 make_plot(spheres)
             print(step)
         do_step(spheres)
-    clear_output(wait=False)
     print(audio)
-    wav.write("test1.wav", 44100, audio)
+    filename = "wavs/test1-s{0}-nspheres{1}-nsteps{2}-g{3}-k{4}.wav".format(size, num_spheres, num_steps, default_g, default_k)
+    wav.write(filename, 44100, audio)
+    print("saved to", filename)
 
-
-spheres = []
-for x in range(3):
-    for y in range(2):
-        spheres.append(Sphere([x,y,0]))
-
-run_steps(spheres, 1000000, skip=10000)
+for num_spheres in range(10):
+    spheres = []
+    for i in range(num_spheres):
+        spheres.append(Sphere([i, i, 0]))
+    run_steps(spheres, num_steps)
